9d6ca5701a20f8f0b64336afeb040b83df889ec8
wangj <jian.wang@du-power.com>
2019-11-07 20:18:00 +0800

    Cache

--- src/main.c
+++ src/main.c
@@ -71,7 +71,7 @@ int main(void)
 			McanComm_TransmitProcess();
 
 			PHY_DP83822_SendDataFrame();
-			PHY_TJA1101_SendEthernetFrame();
+//			PHY_TJA1101_SendEthernetFrame();
 
 			CommUpdateEvent = 0;
 		}
